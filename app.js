fa47c6a6f5e4547a91b6ac77bf4e9141064fff23
Sylvester <sylvester@erbsledet.dk>
2025-10-31 11:29:15 +0100

    with libarys + dotenv setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+const mysql = require('mysql2')
+const express = require('express')
+
+dotenv.config();
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+};
+
+const pool = mysql.createPool(dbConfig);
+
+const app = express();
